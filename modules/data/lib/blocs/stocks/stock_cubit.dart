6e715db37ab03c9b5e576e2fad1dadf9eeca72f2
Tindi Mercy <mutindi@sendyit.com>
2022-10-26 14:01:33 +0300

    feature/adds stock data bloc cubit

--- modules/data/lib/blocs/stocks/stock_cubit.dart
+++ modules/data/lib/blocs/stocks/stock_cubit.dart
@@ -3,9 +3,27 @@ import 'package:data/mappers/stock_data_dto_mapper.dart';
 import 'package:dependencies/dependencies.dart';
 import 'package:domain/repositories/stocks_repository.dart';
 
-class StockCubit extends Cubit<StockState>{
+class StockCubit extends Cubit<StockState> {
   final IStockRepository _stockRepository;
   StockDataDtoMapper dtoMapper;
-  StockCubit(this.dtoMapper,this._stockRepository): super(InitialStockState());
+  final String key = dotenv.env['MARKET_STACK_API_KEY'] ?? 'testKey';
 
+  StockCubit(this.dtoMapper, this._stockRepository)
+      : super(InitialStockState());
+
+  void loadStockMarketData(String sort, String symbols) async {
+    emit(LoadingStockState());
+    Fimber.i('LoadingStockState emitted');
+    try {
+      var response = await _stockRepository.fetchStockMarketData(
+          key, sort, symbols);
+      if (response.success) {
+        Fimber.i('SuccessStockState emitted');
+        emit(SuccessStockState(stockData: response.data!));
+      }
+    } catch (e) {
+      Fimber.e("Error fetching Stock Market Data: $e", ex: e);
+      emit(ErrorStockState());
+    }
+  }
 }
