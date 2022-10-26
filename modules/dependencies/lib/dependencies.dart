44dd323301ecd3138eec640a74f81057d4988bb1
Tindi Mercy <mutindi@sendyit.com>
2022-10-26 09:33:22 +0300

    Set up project
    Set up project

---
+++ modules/dependencies/lib/dependencies.dart
@@ -0,0 +1,13 @@
+library dependencies;
+
+export 'package:dio/dio.dart' hide Headers;
+export 'package:equatable/equatable.dart';
+export 'package:fimber/fimber.dart';
+export 'package:flutter_bloc/flutter_bloc.dart';
+export 'package:flutter_dotenv/flutter_dotenv.dart';
+export 'package:flutter_typeahead/flutter_typeahead.dart';
+export 'package:get_it/get_it.dart';
+export 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+export 'package:injectable/injectable.dart';
+export 'package:retrofit/retrofit.dart' hide Parser, Query;
+export 'package:shimmer/shimmer.dart';
